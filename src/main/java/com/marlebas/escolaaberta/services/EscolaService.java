061b78f70f3d3849b1112e87e481c17fc95dee1a
Marlon <maarlonsantos17@gmail.com>
2026-01-27 12:20:34 -0300

    feat: Cria o EscolaService para a lógica de requisições

--- src/main/java/com/marlebas/escolaaberta/services/EscolaService.java
+++ src/main/java/com/marlebas/escolaaberta/services/EscolaService.java
@@ -1,6 +1,7 @@
 package com.marlebas.escolaaberta.services;
 
 import com.marlebas.escolaaberta.dtos.EscolaResponseDTO;
+import com.marlebas.escolaaberta.mappers.EscolaMapper;
 import com.marlebas.escolaaberta.repositories.EscolaRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,16 @@ import java.util.List;
 public class EscolaService {
 
     private EscolaRepository escolaRepository;
+    private EscolaMapper escolaMapper;
 
-    public EscolaService(EscolaRepository escolaRepository) {
+    public EscolaService(EscolaRepository escolaRepository, EscolaMapper escolaMapper) {
         this.escolaRepository = escolaRepository;
+        this.escolaMapper = escolaMapper;
     }
 
-    public List<EscolaResponseDTO>
+    public List<EscolaResponseDTO> listarTudo(){
+        return escolaRepository.findAll().stream()
+                .map(escolaMapper::map)
+                .toList();
+    }
 }
