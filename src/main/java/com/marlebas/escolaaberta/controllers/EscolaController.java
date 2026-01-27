d2383c8e448267c1d1dd83ad56d0945bd3c05c6e
Marlon <maarlonsantos17@gmail.com>
2026-01-27 13:47:12 -0300

    Refactore: Conserta o endereço para adicionar número e complemento

--- src/main/java/com/marlebas/escolaaberta/controllers/EscolaController.java
+++ src/main/java/com/marlebas/escolaaberta/controllers/EscolaController.java
@@ -2,14 +2,14 @@ package com.marlebas.escolaaberta.controllers;
 
 import com.marlebas.escolaaberta.dtos.EscolaResponseDTO;
 import com.marlebas.escolaaberta.services.EscolaService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("escolas")
+@RestController
+@RequestMapping("/escolas")
 public class EscolaController {
 
     private EscolaService escolaService;
--- src/main/java/com/marlebas/escolaaberta/dtos/EscolaResponseDTO.java
+++ src/main/java/com/marlebas/escolaaberta/dtos/EscolaResponseDTO.java
@@ -1,4 +1,4 @@
 package com.marlebas.escolaaberta.dtos;
 
-public record EscolaResponseDTO(Long id, String nome, String inep, String endereco, String bairro) {
+public record EscolaResponseDTO(Long id, String nome, String inep, String endereco, String numero, String complemento, String bairro) {
 }
